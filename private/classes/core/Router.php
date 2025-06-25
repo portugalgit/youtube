a6d6c1d5312503f1bdbe85e475fc325871706977
portugalgit <portugalgithub@gmail.com>
2025-06-25 11:06:29 +0100

    clonar site youtube aula4 concluido

--- private/classes/core/Router.php
+++ private/classes/core/Router.php
@@ -1,37 +1,60 @@
 <?php
 
-/**
- * Class Router
- */
+namespace Core;
 
+/**
+ * Declaração da classe Router, que será usada para registrar e gerenciar 
+ * rotas de requisições HTTP (como GET e POST).
+ */
  class Router
  {
+    //Atributo privado $routes: armazena todas as rotas da aplicação.
     private $routes = [];
 
+    //Registra uma rota do tipo GET.
     public function get(string $path, string $handler):void
     {
         $this->routes['GET'][] = ['path'=>$path, 'handler'=>$handler];
     }
 
+    //Registra uma rota do tipo POST.
      public function post(string $path, string $handler):void
     {
         $this->routes['POST'][] = ['path'=>$path, 'handler'=>$handler];
     }
 
+    //Responsável por executar a lógica de roteamento.
     public function run()
     {
+        //Verificar se há rotas registradas para esse método
         $method = $_SERVER['REQUEST_METHOD'];
+        $method = rtrim($_SERVER['REQUEST_URI'],'/') ? '/';
 
         if(!isset($this->routes[$method])){
 
             http_response_code(405);
             echo "metodo não permitido";
-            exit;
-        }
+            return;
 
-        
+        }
+            
+            foreach($this->routes[$method] as $route){
+
+                $pattern = preg_replace("#\{[\w-]+\}#", '[\w-]+', $route['path']);
+                $pattern = '#^'.$pattern.'$#';
+
+                if(preg_match($pattern, $pathURL))
+                {
+                    $file = '../private/controllers/'.$route['handler'];
+                    if(file_exists($file))
+                       require $file;
+                    else
+                        redirect('404');
+                    return;
+                }
+            }
+
             http_response_code(404);
-            echo "pagina não encontrado";
-            exit;
+            redirect('404');
     }
  }
